1d405ecc87115945a7e437cbd394d1dba9427150
alessandra martera <ale.martera@gmail.com>
2023-05-15 16:21:51 +0200

    update mod DOM from js

--- js/script.js
+++ js/script.js
@@ -12,25 +12,45 @@ MILESTONE 3
 Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 
 */
+
+
+// Creare i vari DIV con le img 
+// creare l'array con i path delle img
+// chiedere all'utente se ha img da inserire
+
+const arrayPathImmages = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
+
+
+let container = document.getElementById("container");
+// const img = document.querySelector(".item");
+// console.log(items);
+// console.log(next);
+// console.log(next);
+
+
+for ( let i = 0; i < arrayPathImmages.length; i++ ) {
+    
+    // se sei sul primo elemento lo crei e gli assegni la classe active
+
+    container.innerHTML +=    
+    `<div class="item">
+        <img src="${arrayPathImmages[i]}" alt="">
+    </div> `;
+
+    // per tutti gli altri elementi crei solo gli elementi
+}
+
 const next = document.querySelector(".button.next");
 const before = document.querySelector(".button.before");
-const items = document.querySelectorAll("[class ^= img_ ]");
-
-const img = document.querySelector("[class ^= img_ ]");
-console.log(items);
-console.log(next);
-console.log(next);
-
-
-
+const items = document.querySelectorAll(".item");
 let count = 0;
+
+items[count].classList.add("active");
 
 // button next img
 next.addEventListener("click",
     function () {
         if ( count < ( items.length - 1 )) {
-
-
 
             // rimuovo dal div dell'img in cui suono posizionata la classe active
             items[count].classList.remove("active");
